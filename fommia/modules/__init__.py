98f10babc46e67cfd2b4096c9ed8b8ae3475ef3f
Jean Maximilien Cadic <j.cadic@pm.me>
2021-02-25 19:30:12 +0900

    initial commit

---
+++ fommia/modules/__init__.py
@@ -0,0 +1,5 @@
+from .functional import *
+from .dense_motion import DenseMotionNetwork
+from .discriminator import Discriminator
+from .generator  import OcclusionAwareGenerator
+from .keypoint_detector import KPDetector
